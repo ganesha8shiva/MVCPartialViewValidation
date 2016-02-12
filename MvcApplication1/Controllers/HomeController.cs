a4fc51eba2533163c390f7270d558595ab9c1eb9
unknown <ganesha8shiva@gmail.com>
2016-02-12 21:50:15 +0530

    mvc partial view validation

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: /Home/AddUserPartialView
+        public PartialViewResult AddUserPartialView()
+        {
+            ModelState.AddModelError("AddUserViewModel", "Some Error.");
+            return PartialView("AddUserPartialView", new AddUserViewModel());
+        }
+
+
+        // GET: /Home/AddUserInfo
+        [HttpPost]
+        public JsonResult AddUserInfo(AddUserViewModel model)
+        {
+            bool isSuccess = false;
+            if (ModelState.IsValid)
+            {
+                isSuccess = true;
+            }
+
+            //Test Error
+            //int p = 0;
+            //int k = 10 / p;
+            //Test Error
+
+            return Json(new { success = isSuccess, model = model }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ MvcApplication1/Models/AddUserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcApplication1.Models
+{
+    public class AddUserViewModel
+    {
+        [Required(ErrorMessage = "Please enter name !!!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter email address !!!")]
+        [EmailAddress(ErrorMessage = "Please enter valid email address !!!")]
+        public string Email { get; set; }
+    }
+}
